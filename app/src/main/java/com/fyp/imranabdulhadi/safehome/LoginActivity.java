4a28de379901fcbe4446799b553427c3fcae1b5a
Moonking94 <ianhadi96@gmail.com>
2016-10-15 17:55:14 +0800

    15/10/2016, 5:55 PM

--- app/src/main/java/com/fyp/imranabdulhadi/safehome/LoginActivity.java
+++ app/src/main/java/com/fyp/imranabdulhadi/safehome/LoginActivity.java
@@ -2,16 +2,12 @@ package com.fyp.imranabdulhadi.safehome;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
-import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,11 +17,9 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
